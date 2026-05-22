46862a19f13df6d6854c25ed27009747675c9fd2
Thigas827 <thiagocomsilva10@gmail.com>
2026-05-22 19:53:03 -0300

    refatora a classe Container para melhorar a legibilidade do código

--- src/Container.java
+++ src/Container.java
@@ -2,87 +2,86 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Container {
-    private int id;
-    private String tipoCont;
-    private LocalDate dataEntrada;
-    private LocalTime horaEntrada;
-    private String destino;
-    private Navio navioOrigem; 
+   private int id;
+   private String tipoCont;
+   private LocalDate dataEntrada;
+   private LocalTime horaEntrada;
+   private String destino;
+   private Navio navioOrigem;
 
+   public Container(int id, String tipoCont, LocalDate dataEntradaCont, LocalTime horaEntradaCont, String destino,
+         Navio navioOrigem) {
+      this.id = id;
+      this.tipoCont = tipoCont;
+      this.dataEntrada = dataEntradaCont;
+      this.horaEntrada = horaEntradaCont;
+      this.destino = destino;
+      this.navioOrigem = navioOrigem;
+   }
 
-    public Container(int id, String tipoCont, LocalDate dataEntradaCont, LocalTime horaEntradaCont, String destino,
-            Navio navioOrigem) {
-        this.id = id;
-        this.tipoCont = tipoCont;
-        this.dataEntrada = dataEntradaCont;
-        this.horaEntrada = horaEntradaCont;
-        this.destino = destino;
-        this.navioOrigem = navioOrigem;
-    }
+   public Container() {
+   }
 
-    public Container(){
-    }
+   public int getId() {
+      return id;
+   }
 
- public int getId() {
-    return id;
- }
+   public void setId(int id) {
+      this.id = id;
+   }
 
- public void setId(int id) {
-    this.id = id;
- }
+   public String getTipoCont() {
+      return tipoCont;
+   }
 
- public String getTipoCont() {
-    return tipoCont;
- }
+   public void setTipoCont(String tipoCont) {
+      this.tipoCont = tipoCont;
+   }
 
- public void setTipoCont(String tipoCont) {
-    this.tipoCont = tipoCont;
- }
+   public LocalDate getDataEntrada() {
+      return dataEntrada;
+   }
 
- public LocalDate getDataEntrada() {
-    return dataEntrada;
- }
+   public void setDataEntrada(LocalDate dataEntrada) {
+      this.dataEntrada = dataEntrada;
+   }
 
- public void setDataEntrada(LocalDate dataEntrada) {
-    this.dataEntrada = dataEntrada;
- }
+   public LocalTime getHoraEntrada() {
+      return horaEntrada;
+   }
 
- public LocalTime getHoraEntrada() {
-    return horaEntrada;
- }
+   public void setHoraEntrada(LocalTime horaEntrada) {
+      this.horaEntrada = horaEntrada;
+   }
 
- public void setHoraEntrada(LocalTime horaEntrada) {
-    this.horaEntrada = horaEntrada;
- }
+   public String getDestino() {
+      return destino;
+   }
 
- public String getDestino() {
-    return destino;
- }
+   public void setDestino(String destino) {
+      this.destino = destino;
+   }
 
- public void setDestino(String destino) {
-    this.destino = destino;
- }
+   public Navio getNavioOrigem() {
+      return navioOrigem;
+   }
 
- public Navio getNavioOrigem() {
-    return navioOrigem;
- }
+   public void setNavioOrigem(Navio navioOrigem) {
+      this.navioOrigem = navioOrigem;
 
- public void setNavioOrigem(Navio navioOrigem) {
-    this.navioOrigem = navioOrigem;
- 
-    }
+   }
 
- @Override
-    public String toString() {
-        return """
-                Identificação: %d
-                Tipo de container: %s
-                Data de entrada: %s
-                Hora de entrada: %s
-                Destino: %s
-                Navio de origem: %s
-                -------------------------------
-                """.formatted(id, tipoCont, dataEntrada, horaEntrada, destino, navioOrigem.getNomeNavio());
-    }
+   @Override
+   public String toString() {
+      return """
+            Identificação: %d
+            Tipo de container: %s
+            Data de entrada: %s
+            Hora de entrada: %s
+            Destino: %s
+            Navio de origem: %s
+            -------------------------------
+            """.formatted(id, tipoCont, dataEntrada, horaEntrada, destino, navioOrigem.getNomeNavio());
+   }
 
 }
